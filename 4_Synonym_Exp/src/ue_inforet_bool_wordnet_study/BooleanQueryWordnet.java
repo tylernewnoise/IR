1c15c468123df7dfaa4b57b9b365f4a0afa940c5
Benjamin Lorenz <benjamin.lorenz@posteo.de>
2017-01-23 21:17:22 +0100

    added syndex function which gets a HashSet and puts it into a Hashmaps (one for each type). added mergeHashMaps function which merges all Hashmaps into the allSynonyms HashMap.

--- 4_Synonym_Exp/src/ue_inforet_bool_wordnet_study/BooleanQueryWordnet.java
+++ 4_Synonym_Exp/src/ue_inforet_bool_wordnet_study/BooleanQueryWordnet.java
@@ -17,6 +17,13 @@ public class BooleanQueryWordnet {
 	Directory index = new RAMDirectory();
 	// set Analyzer
 	Analyzer myAnalyzer = new StandardAnalyzer();
+
+
+	private static THashMap<String, THashSet<String>> allSynonyms = new THashMap<>();
+	private static THashMap<String, THashSet<String>> adverbs = new THashMap<>();
+	private static THashMap<String, THashSet<String>> adjectives = new THashMap<>();
+	private static THashMap<String, THashSet<String>> verbs = new THashMap<>();
+
 	/**
 	 * DO NOT ADD ADDITIONAL PARAMETERS TO THE SIGNATURE
 	 * OF THE CONSTRUCTOR.
@@ -44,8 +51,115 @@ public class BooleanQueryWordnet {
 		// TODO: insert code here
 	}
 
-	public void synDex (ArrayList<String> tokenList){
+	/***Adds a list of token into the fitting Hashmap
+	 *
+	 * @param tokenList
+	 * @param type
+	 */
+	public static void synDex (THashSet<String> tokenList, int type){
+		THashSet<String> baseFormSynset = new THashSet<>();
+		switch (type) {
 
+			case 1: //data adjective
+				for (String token : tokenList){
+					addToHashmap(adjectives, token, tokenList);
+				}
+				break;
+			case 2: //data adverbs
+				for (String token : tokenList) {
+					addToHashmap(adverbs, token, tokenList);
+				}
+				break;
+			case 3: //data nouns
+				for (String token : tokenList) {
+					addToHashmap(allSynonyms, token, tokenList);
+				}
+				break;
+			case 4: //data verbs
+				for (String token : tokenList) {
+					addToHashmap(verbs, token, tokenList);
+				}
+				break;
+			case 5: //exc adjectives
+				baseFormSynset.clear();
+				baseFormSynset.addAll(tokenList);
+				for (String token : tokenList){
+					baseFormSynset.remove(token);
+					for (String item : baseFormSynset){
+						addToHashmap(adjectives, token, baseFormSynset); // add extensions
+						if (adjectives.get(item) != null){
+							addToHashmap(adjectives, token, adjectives.get(item) ); //add synonyms of extentions
+						}
+					}
+				}
+				break;
+			case 6: //exc adverbs
+				baseFormSynset.clear();
+				baseFormSynset.addAll(tokenList);
+				for (String token : tokenList){
+					baseFormSynset.remove(token);
+					for (String item : baseFormSynset){
+						addToHashmap(adverbs, token, baseFormSynset); // add extensions
+						if (adverbs.get(item) != null){
+							addToHashmap(adverbs, token, adverbs.get(item) ); //add synonyms of extentions
+						}
+					}
+				}
+				break;
+			case 7: //exc nouns
+				baseFormSynset.clear();
+				baseFormSynset.addAll(tokenList);
+				for (String token : tokenList){
+					tokenList.
+					baseFormSynset.remove(token);
+					for (String item : baseFormSynset){
+						addToHashmap(allSynonyms, token, baseFormSynset); // add extensions
+						if (allSynonyms.get(item) != null){
+							addToHashmap(allSynonyms, token, allSynonyms.get(item) ); //add synonyms of extentions
+						}
+					}
+				}
+				break;
+			case 8: // exc verbs
+				baseFormSynset.clear();
+				baseFormSynset.addAll(tokenList);
+				for (String token : tokenList){
+					baseFormSynset.remove(token);
+					for (String item : baseFormSynset){
+						addToHashmap(verbs, token, baseFormSynset); // add extensions
+						if (verbs.get(item) != null){
+							addToHashmap(verbs, token, verbs.get(item) ); //add synonyms of extentions
+						}
+					}
+				}
+				break;
+			default:
+				System.out.println("Type > 8. Undefined type!");
+				break;
+		}
+	}
+
+	/* Adds a  List (HashSet) of synonyms of a single token to a HashMap */
+	private static void addToHashmap(THashMap<String, THashSet<String>> hashMap, String token, THashSet<String> synonymList) {
+		if (hashMap.containsKey(token)) {
+			hashMap.get(token).addAll(synonymList);
+			hashMap.get(token).remove(token);
+		} else {
+			// value has no entry yet, create a list for the value and store film in it
+			THashSet<String> tempHashSet = new THashSet<>();
+			tempHashSet.addAll(synonymList);
+			tempHashSet.remove(token); //remove itself from Hashset
+			hashMap.put(token, tempHashSet);
+		}
+	}
+
+	private static void mergeHashMaps(){
+		allSynonyms.putAll(adjectives);
+		allSynonyms.putAll(adverbs);
+		allSynonyms.putAll(verbs);
+		adjectives.clear();
+		adverbs.clear();
+		verbs.clear();
 	}
 
 	/**
@@ -62,122 +176,7 @@ public class BooleanQueryWordnet {
 	 *          use.
 	 */
 	public void buildIndices(String plotFile) {
-
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(plotFile), StandardCharsets.ISO_8859_1))) {
-
-			String line;  // current input (line)
-			boolean lastLineWasPlot = false; // check if new MV Line starts
-			StringBuilder currentMovieString = new StringBuilder();  // concatenate the plotstring
-
-
-			// Specify a directory (happend earlier) and an index writer
-			IndexWriterConfig config = new IndexWriterConfig(myAnalyzer);
-			// open a new IndexWriter instance
-			IndexWriter writer = new IndexWriter(index, config);
-			// Create a document (and add this document to the index, later):
-			Document doc = new Document();
-
-			while ((line = reader.readLine()) != null){
-				// recognize MV: lines and handle them
-				// new movie starts --> write old currentMovieString to Index and reset doc and currentMovieString
-				if (line.startsWith("MV:") && (lastLineWasPlot)) {
-					doc.add(new TextField("plot", currentMovieString.toString(), Field.Store.YES));
-					writer.addDocument(doc);
-					currentMovieString = new StringBuilder();
-					doc = new Document();
-					lastLineWasPlot = false;
-				}
-				// handle the new movie line
-				if (line.startsWith("MV:") && (!lastLineWasPlot)) {
-					doc.add(new TextField("movieline", line, Field.Store.YES));
-					getTitleTypeYear(doc, line.substring(4, line.length()));
-				}
-				// detect and handle the (new) plotline
-				if (line.startsWith("PL:")) {
-					currentMovieString.append(line.substring(4, line.length()));
-					currentMovieString.append(" ");  // Space necessary at the end of each plotline!
-					lastLineWasPlot = true;
-				}
-			}
-			// after last line was read from file - last write operation has to be triggered!
-			doc.add(new TextField("plot", currentMovieString.toString(), Field.Store.YES));
-			writer.addDocument(doc);
-			// close index writer
-			writer.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
-	}
-
-	// helper Methods
-	private void getTitleTypeYear(Document doc, String movieline) {
-
-		// remove {{SUSPENDED}}
-		if (movieline.contains("{{suspended}}")) {
-			movieline = movieline.replace(" {{suspended}}", "");
-		}
-
-		// +++ series +++
-		if (movieline.startsWith("\"") && !movieline.endsWith("}")) {
-			doc.add((new TextField("type", "series", Field.Store.YES)));
-			parseTitleAndYear(doc, movieline, true);
-		}
-		// +++ episode +++
-		else if (movieline.contains("\"") && movieline.endsWith("}")) {
-			doc.add((new TextField("type", "episode", Field.Store.YES)));
-
-			StringBuilder currentString = new StringBuilder();
-
-			for (int i = movieline.length() - 2; movieline.charAt(i) != '{'; --i) {
-				currentString.append(movieline.charAt(i));
-			}
-
-			doc.add((new TextField("episodetitle",
-				  new StringBuilder(currentString.toString()).reverse().toString(), Field.Store.YES)));
-
-			parseTitleAndYear(doc, movieline.substring(0, movieline.indexOf('{') - 1), true);
-		}
-		// +++ television +++
-		else if (movieline.contains(") (TV)")) {
-			doc.add((new TextField("type", "television", Field.Store.YES)));
-			parseTitleAndYear(doc, movieline.substring(0, movieline.length() - 5), false);
-		}
-		// +++ video +++
-		else if (movieline.contains(") (V)")) {
-			doc.add((new TextField("type", "video", Field.Store.YES)));
-			parseTitleAndYear(doc, movieline.substring(0, movieline.length() - 4), false);
-		}
-		// +++ video game +++
-		else if (movieline.contains(") (VG)")) {
-			doc.add((new TextField("type", "videogame", Field.Store.YES)));
-			parseTitleAndYear(doc, movieline.substring(0, movieline.length() - 5), false);
-		} else {
-			// +++ movie +++
-			doc.add((new TextField("type", "movie", Field.Store.YES)));
-			parseTitleAndYear(doc, movieline, false);
-		}
-	}
-
-	// helper 2
-	private void parseTitleAndYear(Document doc, String movieline, boolean isSeries) {
-		int end = 3;
-		StringBuilder currentString = new StringBuilder();
-
-		for (int i = movieline.length() - 2; movieline.charAt(i) != '('; --i) {
-			if (movieline.charAt(i) >= '0' && movieline.charAt(i) <= '9') {
-				currentString.append(movieline.charAt(i));
-			}
-			end++;
-		}
-		doc.add((new StringField("year", new StringBuilder(currentString.toString()).reverse().toString(), Field.Store.YES)));
-
-		if (isSeries) {
-			doc.add((new TextField("title", movieline.substring(1, movieline.length() - end - 1), Field.Store.YES)));
-		} else {
-			doc.add((new TextField("title", movieline.substring(0, movieline.length() - end), Field.Store.YES)));
-		}
+		// TODO: insert code here
 	}
 
 	/**
@@ -201,27 +200,10 @@ public class BooleanQueryWordnet {
 	 * @return the exact content (in the textual movie plot file) of the title
 	 *         lines (starting with "MV: ") of the documents matching the query
 	 */
-	public Set<String> booleanQuery(String queryString) throws ParseException, IOException {
-		HashSet<String> returnList = new HashSet<>();
-		if (queryString.length() > 0){
-			Query query = new QueryParser("title", myAnalyzer).parse(queryString);
-			IndexReader reader = DirectoryReader.open(index);
-			IndexSearcher searcher = new IndexSearcher(reader);
-			TopDocs docs = searcher.search(query, Integer.MAX_VALUE);
-			ScoreDoc[] hits = docs.scoreDocs;
-			for (int i = 0; i < hits.length; i++){
-				int docID = hits[i].doc;
-				Document d = searcher.doc(docID);
-				returnList.add(d.get("movieline"));
-			}
-		} else {
-			System.err
-				  .println("No query given.");
-			System.exit(-1);
-		}
-		return returnList;
+	public Set<String> booleanQuery(String queryString) {
+		// TODO: insert code here
+		return new HashSet<>();
 	}
-
 
 	/**
 	 * A method for closing any open file handels or a ThreadPool.
