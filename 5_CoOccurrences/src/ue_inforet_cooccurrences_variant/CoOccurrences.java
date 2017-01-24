ab783ac8a685079d226c69c83e67a981ed147e9b
Falko <falko.becker@informatik.hu-berlin.de>
2017-01-24 23:25:44 +0100

    removed

--- 5_CoOccurrences/src/ue_inforet_cooccurrences_variant/CoOccurrences.java
+++ 5_CoOccurrences/src/ue_inforet_cooccurrences_variant/CoOccurrences.java
@@ -1,10 +1,4 @@
 package ue_inforet_cooccurrences_variant;
 
-import gnu.trove.map.hash.THashMap;
-
 public class CoOccurrences {
-
-	public static THashMap<String, Integer> allWords = new THashMap<>();
-	public static THashMap <ue_inforet_cooccurrences.Bigram, Integer> allBigrams = new THashMap<>();
-
 }
