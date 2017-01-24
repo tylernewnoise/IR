251a3d79af4759e3c0321dda03b3c63ea00e31ff
Benjamin Lorenz <benjamin.lorenz@posteo.de>
2017-01-24 18:50:30 +0100

    Added Bigram Class and trove lib.

--- 5_CoOccurrences/src/ue_inforet_cooccurrences_variant/CoOccurrences.java
+++ 5_CoOccurrences/src/ue_inforet_cooccurrences_variant/CoOccurrences.java
@@ -1,5 +1,10 @@
 package ue_inforet_cooccurrences_variant;
 
+import gnu.trove.map.hash.THashMap;
+
 public class CoOccurrences {
+
+	public static THashMap<String, Integer> allWords = new THashMap<>();
+	public static THashMap <ue_inforet_cooccurrences.Bigram, Integer> allBigrams = new THashMap<>();
 
 }
