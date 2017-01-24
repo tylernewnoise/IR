ba699c7576028ee2076f12a40bf7e6327eddb6da
Falko <falko.becker@informatik.hu-berlin.de>
2017-01-24 23:27:04 +0100

    added lucene indices building from assignment 3

--- 4_Synonym_Exp/src/ue_inforet_bool_wordnet_study/BooleanQueryWordnet.java
+++ 4_Synonym_Exp/src/ue_inforet_bool_wordnet_study/BooleanQueryWordnet.java
@@ -29,12 +29,30 @@ import org.apache.lucene.search.IndexSearcher;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
 public class BooleanQueryWordnet {
 	// global accessible index :)
 	Directory index = new RAMDirectory();
 	// set Analyzer
 	Analyzer myAnalyzer = new StandardAnalyzer();
-
 
 	private static THashMap<String, THashSet<String>> allSynonyms = new THashMap<>();
 	private static THashMap<String, THashSet<String>> adverbs = new THashMap<>();
@@ -47,7 +65,6 @@ public class BooleanQueryWordnet {
 	 *
 	 */
 	public BooleanQueryWordnet() {
-		// TODO you may insert code here
 	}
 
 	/**
@@ -208,8 +225,124 @@ public class BooleanQueryWordnet {
 	 *          >http://www.imdb.com/interfaces</a> for personal, non-commercial
 	 *          use.
 	 */
+
 	public void buildIndices(String plotFile) {
-		// TODO: insert code here
+
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(plotFile), StandardCharsets.ISO_8859_1))) {
+
+			String line;  // current input (line)
+			boolean lastLineWasPlot = false; // check if new MV Line starts
+			StringBuilder currentMovieString = new StringBuilder();  // concatenate the plotstring
+
+
+			// Specify a directory (happend earlier) and an index writer
+			IndexWriterConfig config = new IndexWriterConfig(myAnalyzer);
+			// open a new IndexWriter instance
+			IndexWriter writer = new IndexWriter(index, config);
+			// Create a document (and add this document to the index, later):
+			Document doc = new Document();
+
+			while ((line = reader.readLine()) != null){
+				// recognize MV: lines and handle them
+				// new movie starts --> write old currentMovieString to Index and reset doc and currentMovieString
+				if (line.startsWith("MV:") && (lastLineWasPlot)) {
+					doc.add(new TextField("plot", currentMovieString.toString(), Field.Store.YES));
+					writer.addDocument(doc);
+					currentMovieString = new StringBuilder();
+					doc = new Document();
+					lastLineWasPlot = false;
+				}
+				// handle the new movie line
+				if (line.startsWith("MV:") && (!lastLineWasPlot)) {
+					doc.add(new TextField("movieline", line, Field.Store.YES));
+					getTitleTypeYear(doc, line.substring(4, line.length()));
+				}
+				// detect and handle the (new) plotline
+				if (line.startsWith("PL:")) {
+					currentMovieString.append(line.substring(4, line.length()));
+					currentMovieString.append(" ");  // Space necessary at the end of each plotline!
+					lastLineWasPlot = true;
+				}
+			}
+			// after last line was read from file - last write operation has to be triggered!
+			doc.add(new TextField("plot", currentMovieString.toString(), Field.Store.YES));
+			writer.addDocument(doc);
+			// close index writer
+			writer.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
+	// helper Methods
+	private void getTitleTypeYear(Document doc, String movieline) {
+
+		// remove {{SUSPENDED}}
+		if (movieline.contains("{{suspended}}")) {
+			movieline = movieline.replace(" {{suspended}}", "");
+		}
+
+		// +++ series +++
+		if (movieline.startsWith("\"") && !movieline.endsWith("}")) {
+			doc.add((new TextField("type", "series", Field.Store.YES)));
+			parseTitleAndYear(doc, movieline, true);
+		}
+		// +++ episode +++
+		else if (movieline.contains("\"") && movieline.endsWith("}")) {
+			doc.add((new TextField("type", "episode", Field.Store.YES)));
+
+			StringBuilder currentString = new StringBuilder();
+
+			for (int i = movieline.length() - 2; movieline.charAt(i) != '{'; --i) {
+				currentString.append(movieline.charAt(i));
+			}
+
+			doc.add((new TextField("episodetitle",
+				new StringBuilder(currentString.toString()).reverse().toString(), Field.Store.YES)));
+
+			parseTitleAndYear(doc, movieline.substring(0, movieline.indexOf('{') - 1), true);
+		}
+		// +++ television +++
+		else if (movieline.contains(") (TV)")) {
+			doc.add((new TextField("type", "television", Field.Store.YES)));
+			parseTitleAndYear(doc, movieline.substring(0, movieline.length() - 5), false);
+		}
+		// +++ video +++
+		else if (movieline.contains(") (V)")) {
+			doc.add((new TextField("type", "video", Field.Store.YES)));
+			parseTitleAndYear(doc, movieline.substring(0, movieline.length() - 4), false);
+		}
+		// +++ video game +++
+		else if (movieline.contains(") (VG)")) {
+			doc.add((new TextField("type", "videogame", Field.Store.YES)));
+			parseTitleAndYear(doc, movieline.substring(0, movieline.length() - 5), false);
+		} else {
+			// +++ movie +++
+			doc.add((new TextField("type", "movie", Field.Store.YES)));
+			parseTitleAndYear(doc, movieline, false);
+		}
+	}
+
+	// helper 2
+	private void parseTitleAndYear(Document doc, String movieline, boolean isSeries) {
+		int end = 3;
+		StringBuilder currentString = new StringBuilder();
+
+		for (int i = movieline.length() - 2; movieline.charAt(i) != '('; --i) {
+			if (movieline.charAt(i) >= '0' && movieline.charAt(i) <= '9') {
+				currentString.append(movieline.charAt(i));
+			}
+			end++;
+		}
+		doc.add((new StringField("year", new StringBuilder(currentString.toString()).reverse().toString(), Field.Store.YES)));
+
+		if (isSeries) {
+			doc.add((new TextField("title", movieline.substring(1, movieline.length() - end - 1), Field.Store.YES)));
+		} else {
+			doc.add((new TextField("title", movieline.substring(0, movieline.length() - end), Field.Store.YES)));
+		}
 	}
 
 	public String parseQueryString(String originalQuery) {
