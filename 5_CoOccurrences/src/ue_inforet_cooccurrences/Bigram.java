e919aa20d4b9185e50dc97300d7407716a375d56
Falko <falko.becker@informatik.hu-berlin.de>
2017-01-25 14:34:28 +0100

    removed unnecessary contructor, made access package-private, implemented simple hashCode and equals methods

--- 5_CoOccurrences/src/ue_inforet_cooccurrences/Bigram.java
+++ 5_CoOccurrences/src/ue_inforet_cooccurrences/Bigram.java
@@ -2,6 +2,7 @@ package ue_inforet_cooccurrences;
 
 /**
  * Created by benjamin on 24.01.17.
+ * Modified by falko on 25.01.17.
  */
 public class Bigram {
 
@@ -9,43 +10,53 @@ public class Bigram {
 	private String second;
 	private double score;
 
+	public int hashCode() {
+		return this.first.hashCode() + this.second.hashCode();
+	}
 
-	public Bigram(String first, String second) {
-		this.first = first;
-		this.second = second;
-		this.score = -1;
+	@SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+	public boolean equals(Object obj) {
+		return this.first.equals(((Bigram) obj).first) && this.second.equals(((Bigram) obj).second);
 	}
 
 	/***
 	 *
 	 * @return first word of the bigram
 	 */
-	public String getFirst() { return this.first; }
+	String getFirst() {
+		return this.first;
+	}
 
 	/***
 	 *
 	 * @param first sets first word of the bigram
 	 */
-	public void setFirst(String first) { this.first = first; }
+	void setFirst(String first) {
+		this.first = first;
+	}
 
 	/***
 	 *
 	 * @return second word of the bigram
 	 */
-	public String getSecond() { return this.second; }
+	String getSecond() {
+		return this.second;
+	}
 
 	/***
 	 *
 	 * @param second sets second word of the bigram
 	 */
-	public void setSecond(String second) { this.second = second; }
+	void setSecond(String second) {
+		this.second = second;
+	}
 
 	/***
 	 *
 	 * @param score sets score of the bigram, has to be 0 <= score <= 1
 	 */
-	public void setScore(double score){
-		if (score <= 0 && score >= 1){
+	void setScore(double score) {
+		if (score <= 0 && score >= 1) {
 			this.score = score;
 		} else {
 			System.out.println("Score: " + score + " of the Bigram <" + this.first + "," + this.second + "> is out of Range");
@@ -56,9 +67,9 @@ public class Bigram {
 	 *
 	 * @return score of the bigram if already set. if not set yet it returns 0 and a warning.
 	 */
-	public double getScore(){
-		if (this.score < 0){
-			System.out.println("Score of the Bigram <" + this.first + "," + this.second + "> has not been set yet." );
+	double getScore() {
+		if (this.score < 0) {
+			System.out.println("Score of the Bigram <" + this.first + "," + this.second + "> has not been set yet.");
 			return 0;
 		} else {
 			return this.score;
