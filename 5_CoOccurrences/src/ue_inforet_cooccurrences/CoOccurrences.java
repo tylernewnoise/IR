cf07b17f2a325d4b0aa3b27192611aba06ab9d00
Falko <falko.becker@informatik.hu-berlin.de>
2017-01-25 14:36:07 +0100

    changed allBigrams type, implemented: wordcounting, bigramm-building for title-line

--- 5_CoOccurrences/src/ue_inforet_cooccurrences/CoOccurrences.java
+++ 5_CoOccurrences/src/ue_inforet_cooccurrences/CoOccurrences.java
@@ -1,6 +1,5 @@
 package ue_inforet_cooccurrences;
 
-import gnu.trove.map.hash.THashMap;
 import gnu.trove.map.hash.TObjectIntHashMap;
 import gnu.trove.set.hash.THashSet;
 
@@ -8,15 +7,17 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class CoOccurrences {
 
 	private TObjectIntHashMap<String> allWords = new TObjectIntHashMap<>();
-	private THashMap <Bigram, Integer> allBigrams = new THashMap<>();
+	private TObjectIntHashMap<Bigram> allBigrams = new TObjectIntHashMap<>();
 	private THashSet<String> stopWords = new THashSet<>();
 
-	private CoOccurrences(){}
+	private CoOccurrences() {
+	}
 
 	// parse and safe the stop-words
 	private void parseStopWords() {
@@ -31,7 +32,7 @@ public class CoOccurrences {
 		}
 	}
 
-	private void parsePlotList(String plotFile){
+	private void parsePlotList(String plotFile) {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(plotFile),
 			StandardCharsets.ISO_8859_1))) {
 			String line;
@@ -44,8 +45,12 @@ public class CoOccurrences {
 						toLowerCase(), " .,:!?", false);
 					while (st.hasMoreTokens()) {
 						String token = st.nextToken();
-						allWords.adjustOrPutValue(token, 1, 1);
+						if (!stopWords.contains(token)) {
+							allWords.adjustOrPutValue(token, 1, 1);
+						}
 						// TODO add occurrences and check for stop words and stuff
+						// check if line before was MV:Line or plot. if plot, take the last word
+						// as well
 					}
 				}
 			}
@@ -98,15 +103,41 @@ public class CoOccurrences {
 			st = new StringTokenizer(title.substring(0, title.length() - end + 1), " .,:!?", false);
 		}
 
+		// put all the title words in a list.
+		ArrayList<String> titleTokens = new ArrayList<>();
 		while (st.hasMoreTokens()) {
-			String token = st.nextToken();
-			//TODO check cooccurrences and stop words
-			allWords.adjustOrPutValue(token, 1, 1);
+			titleTokens.add(st.nextToken());
+		}
+
+		// if there is only one word in the title add the word to the overall wordcount
+		if (titleTokens.size() < 2) {
+			// count it only if it is not a stop word
+			if (!stopWords.contains(titleTokens.get(0))) {
+				allWords.adjustOrPutValue(titleTokens.get(0), 1, 1);
+			}
+		} else {
+			// count the words in the title (excluding stop words)
+			for (String token : titleTokens) {
+				if (!stopWords.contains(token)) {
+					allWords.adjustOrPutValue(token, 1, 1);
+				}
+			}
+			// run through the list, build the bigrams if possible and count it
+			for (int i = 0; i + 1 < titleTokens.size(); ++i) {
+				// check for stop words
+				if (!stopWords.contains(titleTokens.get(i)) && !stopWords.contains(titleTokens.get(i + 1))) {
+					Bigram bg = new Bigram();
+					bg.setFirst(titleTokens.get(i));
+					bg.setSecond(titleTokens.get(i + 1));
+					allBigrams.adjustOrPutValue(bg, 1, 1);
+				}
+			}
 		}
 	}
 
 	public static void main(String[] args) {
 		CoOccurrences co = new CoOccurrences();
+
 		if (args.length < 1) {
 			System.err.println("usage: java -jar CoOccurrences.jar <plot list file>");
 			System.exit(-1);
@@ -114,5 +145,7 @@ public class CoOccurrences {
 
 		System.out.println("Parsing stop.words...");
 		co.parseStopWords();
+		System.out.println("Parsing plot.list...");
+		co.parsePlotList(args[0]);
 	}
 }
