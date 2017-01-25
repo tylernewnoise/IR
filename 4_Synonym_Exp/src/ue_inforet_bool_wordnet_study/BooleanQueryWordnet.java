2d658c8ff4c6fe6dd81ea3067dfef325bf2b6260
Christoph Bussenius <life4life@freenet.de>
2017-01-25 20:07:05 +0100

    removed unnecessary drop of line.... and split starts at index 17 not 18 now

--- 4_Synonym_Exp/src/ue_inforet_bool_wordnet_study/BooleanQueryWordnet.java
+++ 4_Synonym_Exp/src/ue_inforet_bool_wordnet_study/BooleanQueryWordnet.java
@@ -63,13 +63,9 @@ public class BooleanQueryWordnet {
 					continue;
 				}
 				// split read words
-				String[] justSplit = StringUtils.substring(line, 18).split( Pattern.quote( " 0 " ) );
+				String[] justSplit = StringUtils.substring(line, 17).split( Pattern.quote( " 0 " ) );
+
 				// read/process the split words
-
-				if (justSplit[0].contains("_")) {   // does this one word is made of >=2 tokens?
-					continue; // leave this line alone
-				}
-
 				THashSet<String> tmp =  new THashSet<>();
 				for (int i=0; i < wordCount; i++){
 					if (justSplit[i].contains("_")) {   // does this one word is made of >=2 tokens?
@@ -109,9 +105,6 @@ public class BooleanQueryWordnet {
 
 				// else
 				// read/process the split words
-				if (justSplit[0].contains("_")) {   // does this one word is made of >=2 tokens?
-					continue; // leave this line alone
-				}
 
 				THashSet<String> tmp =  new THashSet<>();
 				for (int i=0; i < wordCount; i++){
