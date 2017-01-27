5027a70ed267adc7d282f368c51a64f9057e5290
Falko <falko.becker@informatik.hu-berlin.de>
2017-01-27 11:06:33 +0100

    bigram class

---
+++ 5_CoOccurrences/src/ue_inforet_cooccurrences_std/Bigram.java
@@ -0,0 +1,68 @@
+package ue_inforet_cooccurrences_std;
+
+/**
+ * Created by benjamin on 24.01.17.
+ * Modified by falko on 25.01.17.
+ */
+public class Bigram {
+
+	private String first;
+	private String second;
+	private double score;
+
+	Bigram(String first, String second) {
+		this.first = first;
+		this.second = second;
+		this.score = -1;
+	}
+
+	public int hashCode() {
+		return this.first.hashCode() + this.second.hashCode();
+	}
+
+	@SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+	public boolean equals(Object obj) {
+		return this.first.equals(((Bigram) obj).first) && this.second.equals(((Bigram) obj).second);
+	}
+
+	/***
+	 *
+	 * @return first word of the bigram
+	 */
+	String getFirst() {
+		return this.first;
+	}
+
+	/***
+	 *
+	 * @return second word of the bigram
+	 */
+	String getSecond() {
+		return this.second;
+	}
+
+	/***
+	 *
+	 * @param score sets score of the bigram, has to be 0 <= score <= 1
+	 */
+	void setScore(double score) {
+		if (score <= 0 && score >= 1) {
+			this.score = score;
+		} else {
+			System.out.println("Score: " + score + " of the Bigram <" + this.first + "," + this.second + "> is out of Range");
+		}
+	}
+
+	/***
+	 *
+	 * @return score of the bigram if already set. if not set yet it returns 0 and a warning.
+	 */
+	double getScore() {
+		if (this.score < 0) {
+			System.out.println("Score of the Bigram <" + this.first + "," + this.second + "> has not been set yet.");
+			return 0;
+		} else {
+			return this.score;
+		}
+	}
+}
