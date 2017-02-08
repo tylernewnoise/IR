5fe07e1988c5e923a4f3e4573d007bea767db3ad
Falko <falko.becker@informatik.hu-berlin.de>
2017-02-08 13:39:44 +0100

    removed if statements

--- 5_CoOccurrences/src/ue_inforet_cooccurrences/Bigram.java
+++ 5_CoOccurrences/src/ue_inforet_cooccurrences/Bigram.java
@@ -2,7 +2,7 @@ package ue_inforet_cooccurrences;
 
 /**
  * Created by benjamin on 24.01.17.
- * Modified by falko on 25.01.17.
+ * Modified by falko on 08.02.17.
  */
 public class Bigram {
 
@@ -10,7 +10,7 @@ public class Bigram {
 	private String second;
 	private double score;
 
-	public Bigram(String first, String second) {
+	Bigram(String first, String second) {
 		this.first = first;
 		this.second = second;
 		this.score = -1;
@@ -43,26 +43,17 @@ public class Bigram {
 
 	/***
 	 *
-	 * @param score sets score of the bigram, has to be 0 <= score <= 1
+	 * @param score sets score of the bigram
 	 */
 	void setScore(double score) {
-		if (score <= 0 && score >= 1) {
 			this.score = score;
-		} else {
-			System.out.println("Score: " + score + " of the Bigram <" + this.first + "," + this.second + "> is out of Range");
-		}
 	}
 
 	/***
 	 *
-	 * @return score of the bigram if already set. if not set yet it returns 0 and a warning.
+	 * @return score of the bigram
 	 */
 	double getScore() {
-		if (this.score < 0) {
-			System.out.println("Score of the Bigram <" + this.first + "," + this.second + "> has not been set yet.");
-			return 0;
-		} else {
 			return this.score;
-		}
 	}
 }
